5b22280cdeacc5f2b66220476c53122e35fe4d80
Dang Doan <dangdoan@adt.com>
2018-05-26 17:02:58 -0500

    First commit

---
+++ app/src/main/java/com/dangdoan/app/weathermap/StartActivity.java
@@ -0,0 +1,12 @@
+package com.dangdoan.app.weathermap;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
